40c50ee77a7a5d7fc3ebf026e439f41183672aba
gart09 <gart09@naver.com>
2026-09-15 14:18:38 +0900

    [Feat] GlobalException lombok 사용

--- src/main/java/com/myapp/common/GlobalException.java
+++ src/main/java/com/myapp/common/GlobalException.java
@@ -1,9 +1,12 @@
 package com.myapp.common;
 
+import lombok.Getter;
+
 /**
  * 서비스 전반에서 사용할 비즈니스 예외입니다.
  * HTTP 표현 방식은 {@link GlobalExceptionHandler}가 담당합니다.
  */
+@Getter
 public class GlobalException extends RuntimeException {
 
     private final ErrorCode errorCode;
@@ -15,9 +18,5 @@ public class GlobalException extends RuntimeException {
     public GlobalException(ErrorCode errorCode, String message) {
         super(message);
         this.errorCode = errorCode;
-    }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
     }
 }
