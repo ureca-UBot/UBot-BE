ba14816613d6abe05e4418c9710cd64eb5cbe2d2
유민하 <bi6480127@gmail.com>
2026-09-17 13:49:36 +0900

    [Chore] DB 및 애플리케이션 기본 시간대 KST 설정 (#23)

--- src/main/java/com/ubot/UbotBeApplication.java
+++ src/main/java/com/ubot/UbotBeApplication.java
@@ -2,11 +2,13 @@ package com.ubot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class UbotBeApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(UbotBeApplication.class, args);
     }
 }
