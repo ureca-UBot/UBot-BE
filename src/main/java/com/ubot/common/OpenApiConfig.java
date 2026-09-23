851273512e8a879ffc7d2b782ef61dc817675981
Catverdose <eongpup@gmail.com>
2026-09-23 13:53:15 +0900

    chore: swagger 추가

--- src/main/java/com/ubot/common/OpenApiConfig.java
+++ src/main/java/com/ubot/common/OpenApiConfig.java
@@ -6,9 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
 @Configuration
 public class OpenApiConfig {
 
